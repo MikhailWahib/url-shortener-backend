0782b13a1e4c2d882916b37fafd564b108ec1494
MikhailWahib <mikhailwahib1@gmail.com>
2023-11-17 17:56:04 +0200

    Refactor shortUrlController

--- controllers/shortUrlController.ts
+++ controllers/shortUrlController.ts
@@ -5,6 +5,7 @@ import { validationResult } from "express-validator"
 export const handleShortenUrl = async (req: Request, res: Response) => {
 	try {
 		const { url } = req.body
+		const userId = req.userId
 
 		const errors = validationResult(req)
 
@@ -12,7 +13,21 @@ export const handleShortenUrl = async (req: Request, res: Response) => {
 			return res.status(400).json({ errors: errors.array() })
 		}
 
-		const result = await insertUrlToDB(url)
+		const shortUrlCode = Math.random().toString(36).slice(2, 8)
+
+		const shortUrl = `${req.protocol}://${req.get("host")}/s/${shortUrlCode}`
+
+		// check if the url starts with 'https://'
+		let insertedOriginalUrl = url
+		if (!insertedOriginalUrl.startsWith("https://")) {
+			insertedOriginalUrl = `https://${insertedOriginalUrl}`
+		}
+
+		const result = await insertUrlToDB(
+			insertedOriginalUrl,
+			userId!,
+			shortUrlCode
+		)
 
 		if (result.rows.length === 0) {
 			return res.status(500).json({ error: "Internal server error" })
@@ -20,9 +35,7 @@ export const handleShortenUrl = async (req: Request, res: Response) => {
 
 		res.status(201).json({
 			id: result.rows[0].id,
-			shortUrl: `${req.protocol}://${req.get("host")}/s/${
-				result.rows[0].short_url
-			}`,
+			shortUrl: shortUrl,
 			originalUrl: result.rows[0].original_url,
 		})
 	} catch (error) {
