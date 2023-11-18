4297227c0097cd894b1af1ed4dc09cb4f853ae2a
MikhailWahib <mikhailwahib1@gmail.com>
2023-11-18 14:10:13 +0200

    Add morgan

--- middlewares/logger.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express"
-export const logger = (req: Request, res: Response, next: NextFunction) => {
-	console.log(
-		`${req.method} | ${req.path} | ${new Date().toUTCString()} | ${
-			res.statusCode
-		}`
-	)
-	next()
-}
--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express, { Router, Request, Response } from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import { logger } from "./middlewares/logger"
+import morgan from "morgan"
 
 import routes from "./routes"
 
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(cookieParser())
-app.use(logger)
+app.use(morgan("common"))
 
 // routes
 app.use("/api/v1", routes)
